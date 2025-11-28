1862291217f6aa4cac598b100632342cd310577b
waghmarepranav2006 <pranavwaghmare010206@gmail.com>
2025-11-28 21:23:07 +0530

    added ninja api

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'SpendSplit.apps.SpendsplitConfig',
+    'SpendSplit',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://192.168.0.107:3000"
 ]
 
 ROOT_URLCONF = 'DjangoProject.urls'
